f2319b9df89fe67a217f746d2c50008f134d0f6d
Jesper Persson <jesper.persson@outlook.com>
2018-02-21 13:48:41 +0100

    Tint objects far away with the sky color

--- FlightSimulator/Source/fragmentShader.glsl
+++ FlightSimulator/Source/fragmentShader.glsl
@@ -13,6 +13,12 @@ smooth in vec3 fragment_out;
 
 out vec4 gl_Color;
 
+vec3 saturate(vec3 color, float saturate) {
+    const vec3 w = vec3(0.2125, 0.7154, 0.0721);
+    vec3 intensity = vec3(dot(color, w));
+    return mix(intensity, color, saturate);
+}
+
 void main() {
 	// Spotlight
 	/*vec3 lightPos = vec3(5, 6, 5);
@@ -40,22 +46,25 @@ void main() {
 		vec4 terrain1 = texture(tex, texture_out);
 		vec4 terrain2 = texture(tex2, texture_out);
 		vec4 terrain3 = texture(tex3, texture_out);
-		vec4 splat = texture(tex4, texture_out / 80);
+		vec4 splat = texture(tex4, texture_out / 100);
 		gl_Color = (splat.x * terrain1 + splat.y * terrain2 + splat.z * terrain3) * diffuse;
 
 	} else {
 		gl_Color = texture(tex, texture_out) * diffuse;
 	}
-	
-	gl_Color.w = 1;
 
 	// Fade far away objects
 	mat4 invView = inverse(worldToView);
 	vec3 cameraPos = vec3(invView[3][0], invView[3][1], invView[3][2]);
 	float camDistance = length(cameraPos - fragment_out);
-	float distanceStartFade = 500;
-	float distanceEndFade = 800;
+	float distanceStartFade = 10000;
+	float distanceEndFade = 100000;
 	if (camDistance > distanceStartFade) {
-		gl_Color.w = mix(1, 0, (camDistance - distanceStartFade) / (distanceEndFade - distanceStartFade));
+		float saturation = clamp((camDistance - distanceStartFade) / (distanceEndFade - distanceStartFade), 0, 1);
+		gl_Color = vec4(saturate(gl_Color.xyz, 1 - saturation), 1);
+		gl_Color = mix(gl_Color, vec4(0.27, 0.43, 0.66, 1), saturation);
 	}
+
+	gl_Color.w = 1;
 }
+
